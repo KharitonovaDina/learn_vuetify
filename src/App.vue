b2c910ab387b4b50229ad57e89fb79366838639e
KharitonovaDina <d.i.haritonova@yandex.ru>
2023-03-30 13:52:32 +0700

    add component NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,14 @@
 <template>
   <v-app>
-    <v-card
-      height="100vh"
-      tile="false"
-    >
-      <v-navigation-drawer
-        class="#1E1717"
-        dark
-        permanent
-      >
-        <v-list>
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-
-        <template v-slot:append>
-          <div class="pa-2">
-            <v-btn block>
-              Logout
-            </v-btn>
-          </div>
-        </template>
-      </v-navigation-drawer>
-    </v-card>
+    <NavBar/>
   </v-app>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue';
 
 export default {
   name: 'App',
-
-  data() {
-    return {
-      items: [
-        {
-          title: 'Dashboard',
-          icon: 'mdi-view-dashboard',
-        },
-        {
-          title: 'Account',
-          icon: 'mdi-account-box',
-        },
-        {
-          title: 'Admin',
-          icon: 'mdi-gavel',
-        },
-      ],
-    };
-  },
+  components: { NavBar },
 };
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-navigation-drawer
+    tag="nav"
+    color="#1E1717"
+    dark
+    width="235px"
+  >
+    <h3 class="text-h4 white--text px-2 text-uppercase">
+      Filmoteka
+    </h3>
+    <v-list
+      nav
+    >
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+
+  data() {
+    return {
+      items: [
+        {
+          title: 'Films',
+          icon: 'mdi-movie',
+        },
+        {
+          title: 'Serials',
+          icon: 'mdi-video',
+        },
+        {
+          title: 'TV-shows',
+          icon: 'mdi-television-box',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
